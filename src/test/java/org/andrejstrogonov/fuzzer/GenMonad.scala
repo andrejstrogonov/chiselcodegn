97ade8705fba8437d04f10f9c4a7de88779803b4
andrejstrogonov <strogonovandrej@hotmail.com>
2024-12-06 22:03:54 +0300

    Refactoring fuzzer tests

--- src/test/java/org/andrejstrogonov/fuzzer/GenMonad.scala
+++ src/test/java/org/andrejstrogonov/fuzzer/GenMonad.scala
@@ -3,19 +3,21 @@ package org.andrejstrogonov.fuzzer
 import org.scalacheck.{Gen, GenMonad, Properties}
 
 object GenMonad {
-  implicit def scalaCheckGenMonadInstance: GenMonad[Gen] = new GenMonad[Gen] with GenMonadTrait {
-    override def flatMap[A, B](a: Gen[A])(f: A => Gen[B]): Gen[B] = a.flatMap(f)
+  given scalaCheckGenMonadInstance with 
 
-    override def map[A, B](a: Gen[A])(f: A => B): Gen[B] = a.map(f)
+  def genMonad: GenMonad[Gen] = new GenMonad[Gen] {
+    def flatMap[A, B](a: Gen[A])(f: A => Gen[B]): Gen[B] = a.flatMap(f)
 
-    override def choose(min: Int, max: Int): Gen[Int] = Gen.choose(min, max)
+    def map[A, B](a: Gen[A])(f: A => B): Gen[B] = a.map(f)
 
-    override def oneOf[A](items: A*): Gen[A] = Gen.oneOf(items)
+    def choose(min: Int, max: Int): Gen[Int] = Gen.choose(min, max)
 
-    override def const[A](c: A): Gen[A] = Gen.const(c)
+    def oneOf[A](items: A*): Gen[A] = Gen.oneOf(items)
 
-    override def widen[A, B >: A](ga: Gen[A]): Gen[B] = ga
+    def const[A](c: A): Gen[A] = Gen.const(c)
 
-    override def generate[A](ga: Gen[A]): A = ga.sample.get
+    def widen[A, B >: A](ga: Gen[A]): Gen[B] = ga
+
+    def generate[A](ga: Gen[A]): A = ga.sample.get
   }
 }
